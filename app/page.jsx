a51cd7b089cbcfd3fe7c10c88ba131e76f0817fa
Knowledge-JO <megamindtheincredible@gmail.com>
2023-09-22 20:41:31 +0100

    blockpay-payment-site

--- app/page.jsx
+++ app/page.jsx
@@ -193,13 +193,7 @@ const PaymentPage = () => {
                 </div>
               </Link> */}
               <button
-                className={`border border-gray-200 px-4 py-2 rounded-md text-gray-100 ${
-                  connecting
-                    ? "bg-gray-500"
-                    : wallet
-                    ? "bg-red-500 border border-none hover:bg-red-700"
-                    : "bg-blue-500 border border-none hover:bg-blue-700"
-                }`}
+                className={`border border-gray-200 px-4 py-2 rounded-md text-gray-100 bg-blue-500 border border-none hover:bg-blue-700`}
                 disabled={connecting}
                 onClick={() => (wallet ? disconnect(wallet) : connect())}
               >
